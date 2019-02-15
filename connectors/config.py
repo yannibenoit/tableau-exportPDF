9e57ee8a5dbe8ec1914fddc80d2a91973606a887
Yanni Iyeze <yanni-benoit-iyeze@MacBook-Pro-de-Yanni.local>
2019-02-15 18:06:30 +0100

    change API version

--- connectors/config.py
+++ connectors/config.py
@@ -4,13 +4,13 @@ TABLEAU SERVER & Online credentials
 
 API = {
     'online': {
-        'url':'https://eu-west-1a.online.tableau.com',
-        'site': 'saegus',
+        'url':'',
+        'site': '',
         'version': 3.2
 
     },
     'server': {
-        'url': 'http://37.187.170.119:13101',
+        'url': '',
         'version': 3.2,
         'site': ''
     }
